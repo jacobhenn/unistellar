36f5681e7c670ebf5cbcb31927f0192dc26eb39e
Jacob Henn <windowWeasel@outlook.com>
2024-09-28 22:44:16 -0700

    remove examples from routes.rs in favor of the wiki as a single reference

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -112,21 +112,6 @@ where
 
 /// GET "/api/user/<id>": data of user with a given user ID. If a user with the given ID does
 /// not exist, returns 404.
-///
-/// Example:
-/// ```json
-/// {
-///   "id": "01J7YZ7MC3P44547KT11KHXGJV",
-///   "name": {
-///     "first": "Jacob",
-///     "last": "Henn"
-///   },
-///   "username": "jacobhenn",
-///   "university": "01J7YZ7MBVRK9B50WM6E6ZABJ0",
-///   "major": "01J7YZ7MBXM1C8R69K8Y087K0C",
-///   "grad_year": 2026
-/// }
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>", rank = 1)]
 pub async fn user(
@@ -146,11 +131,6 @@ pub async fn user(
 
 /// GET "/api/user/<id>/following": list of IDs of users that the given user is following. If a user
 /// with the given id does not exist, returns 404.
-///
-/// Example:
-/// ```json
-/// ["01J7YXMV1FSVAERRYEPR93NRX9","01J7YXMV1FZ94VHC13RCTRZM09"]
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>/following")]
 pub async fn user_following(
@@ -168,11 +148,6 @@ pub async fn user_following(
 
 /// GET "/api/user/<id>/followers": list of IDs of users that follow the given user. If a user with
 /// the given id does not exist, returns 404.
-///
-/// Example:
-/// ```json
-/// ["01J7YXMV1FSVAERRYEPR93NRX9","01J7YXMV1FZ94VHC13RCTRZM09"]
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>/followers")]
 pub async fn user_followers(
@@ -190,11 +165,6 @@ pub async fn user_followers(
 
 /// GET "/api/user/<id>/courses": list of IDs of courses that the given user is taking. If a user
 /// with the given id does not exist, returns 404.
-///
-/// Example:
-/// ```json
-/// ["01J7YXMV1FSVAERRYEPR93NRX9","01J7YXMV1FZ94VHC13RCTRZM09"]
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>/courses")]
 pub async fn user_courses(
@@ -211,14 +181,6 @@ pub async fn user_courses(
 }
 
 /// GET "/api/user/<id>/stats": statistics of the given user related to their activity.
-///
-/// Example:
-/// ```json
-/// {
-///   "assignments_completed": 1,
-///   "secs_worked": 1500
-/// }
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>/stats", rank = 3)]
 pub async fn user_stats(
@@ -238,17 +200,6 @@ pub async fn user_stats(
 
 /// GET "/api/user/<id>/assignment_statuses": lists of IDs of assignments planned, in progress, and
 /// completed by the user with the given ID.
-///
-/// Example:
-/// ```json
-/// {
-///   "assignments_planning": [],
-///   "assignments_in_progress": [],
-///   "assignments_completed": [
-///     "01J8Y4FVJ4X6KZ8MDQZ7Y74KJM"
-///   ]
-/// }
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>/assignment_statuses", rank = 3)]
 pub async fn user_assignment_statuses(
@@ -281,11 +232,6 @@ pub async fn user_assignment_statuses(
 
 /// GET "/api/uni/<id>/students": list of IDs of users who attend the given university. If
 /// the given university ID does not exist, returns 404.
-///
-/// Example:
-/// ```json
-/// ["01J7YZ7MC3C49R19BHX6DTPGJ2","01J7YZ7MC3P44547KT11KHXGJV"]
-/// ```
 #[instrument(skip(state))]
 #[get("/uni/<id_param>/students")]
 pub async fn uni_students(
@@ -303,22 +249,6 @@ pub async fn uni_students(
 
 /// GET "/api/course/search/<search>": list of courses whose names match the given search string,
 /// sorted in order of search relevance.
-///
-/// Example:
-/// ```json
-/// [
-///   {
-///     "id": "01J88T2H1G3XHEN8J9ME231QGM",
-///     "name": "Intro to Life Science",
-///     "code": "BIO 1110"
-///   },
-///   {
-///     "id": "01J88T2H1HTXB53ZHRQ375RMJF",
-///     "name": "Fundamentals of Data Science",
-///     "code": "CS 2410"
-///   }
-/// ]
-/// ```
 #[instrument(skip(state))]
 #[get("/course/search/<search_param>")]
 pub async fn course_search(
@@ -347,20 +277,6 @@ pub async fn course_search(
 
 /// GET "/api/assignment/search/<search>": list of assignments whose names match the given search
 /// string, sorted in order of search relevance.
-///
-/// Example:
-/// ```json
-/// [
-///   {
-///     "id": "01J88T2H1G3XHEN8J9ME231QGM",
-///     "name": "HW 2",
-///   },
-///   {
-///     "id": "01J88T2H1HTXB53ZHRQ375RMJF",
-///     "name": "Quiz 1",
-///   }
-/// ]
-/// ```
 #[instrument(skip(state))]
 #[get("/assignment/search/<search_param>")]
 pub async fn assignment_search(
@@ -387,20 +303,6 @@ pub async fn assignment_search(
 
 /// GET "/api/user/search/<search>": list of users whose names match the given search string,
 /// sorted in increasing order of fuzzy distance with the search string.
-///
-/// Example:
-/// ```json
-/// [
-///   {
-///     "id": "01J88T2H1HJSC58YDZTAK07CM2",
-///     "username": "choobipanda",
-///     "name": {
-///       "first": "Amy",
-///       "last": "Nguyen"
-///     }
-///   }
-/// ]
-/// ```
 #[instrument(skip(state))]
 #[get("/user/search/<search_param>", rank = 2)]
 pub async fn user_search(
@@ -433,20 +335,6 @@ pub async fn user_search(
 
 /// GET "/api/uni/search/<search>": list of universities whose names match the given search
 /// string, sorted in increasing order of fuzzy distance with the search string.
-///
-/// Example:
-/// ```json
-/// [
-///   {
-///     "id": "01J88T2H1FKEY2GS8VTZEZRESM",
-///     "name": "Lancaster University"
-///   },
-///   {
-///     "id": "01J88T2H1CF3QF9BAAA5TG07TB",
-///     "name": "Cal Poly Pomona"
-///   }
-/// ]
-/// ```
 #[instrument(skip(state))]
 #[get("/uni/search/<search_param>", rank = 2)]
 pub async fn uni_search(
@@ -471,20 +359,6 @@ pub async fn uni_search(
 
 /// GET "/api/major/search/<search>": list of majors whose names match the given search
 /// string, sorted in increasing order of fuzzy distance with the search string.
-///
-/// Example:
-/// ```json
-/// [
-///   {
-///     "id": "01J88T2H1FSZ235CW69M9TA4PM",
-///     "name": "Computer Science"
-///   },
-///   {
-///     "id": "01J88T2H1F400Y58VFHYXAWTD0",
-///     "name": "Mathematics"
-///   }
-/// ]
-/// ```
 #[instrument(skip(state))]
 #[get("/major/search/<search_param>", rank = 2)]
 pub async fn major_search(
@@ -509,35 +383,6 @@ pub async fn major_search(
 
 /// GET "/api/user/<id>/activity": list of activities registered by the given user, sorted in
 /// decreasing order of recency.
-///
-/// Example:
-/// ```json
-/// [
-///   {
-///     "time": "2024-09-21T04:25:56.585787586Z",
-///     "course": {
-///       "id": "01J89D8KK39ERH28YH788WJR0R",
-///       "code": "CS 2600"
-///     },
-///     "assignment": "Quiz 1",
-///     "data": {
-///       "kind": "Completed"
-///     }
-///   },
-///   {
-///     "time": "2024-09-21T04:25:56.585461814Z",
-///     "course": {
-///       "id": "01J89D8KK39ERH28YH788WJR0R",
-///       "code": "CS 2600"
-///     },
-///     "assignment": "Quiz 1",
-///     "data": {
-///       "kind": "WorkedOn",
-///       "duration_secs": 1500
-///     }
-///   }
-/// ]
-/// ```
 #[instrument(skip(state))]
 #[get("/user/<id_param>/activity", rank = 3)]
 pub async fn user_activity(
